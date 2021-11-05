1c1c6a0792382e6ccab63e5a40ece34f9499a437
Robson Caldas <bag@rcaldas.com>
2021-11-05 10:25:22 -0300

    Clean logs, debug and watch mode. Update Readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# obd2
+Log and Analyse a vehicle reading OBD2 data from wifi or bluetooth.
+---
+
+***It creates a csv .log file with the starting date and result of each obd2 command present in 'commands' list.***
+
+---
+
+### How to use
+
+You need a obd2 scanner like ELM-327, I used a Wifi one.
+
+- Set car name and list of obd2 commands to log
+- Verify connection
+  - It's set to connect a wifi device in default '192.168.0.10:35000' address
+  - If bluetooth, configure properly, see [Python-OBD Docs](https://python-obd.readthedocs.io/en/latest/Connections/)
+- To start logging run ```python3 obd2.py```
+- To stop logging press ```CTRL+C```
+- A new file with start date will be in 'logs' directory
+
+---
+
+### What's next
+
+- Become a portable app with Kivy
+- Calculate differential ratio from velocity to estimate actual gear
+- Record in time-series format
+- Log only O2 sensors to analyse with more precision
+- ...
--- obd2.py
+++ obd2.py
@@ -4,17 +4,8 @@ from datetime import datetime
 import time
 from tinydb import TinyDB, Query
 
-# db = TinyDB('logs/db.json')
-# db.insert({'type': 'apple', 'count': 7})
-# db.all()
-# for item in db:
-#     print(item)
-# Fruit = Query()
-# db.search(Fruit.type == 'peach')
-# db.update({'count': 10}, Fruit.type == 'apple')
-# db.remove(Fruit.count < 5)
 
-car = 'Omega_Fresh'
+car = 'Omega'
 commands = [
     'RPM',
     'COOLANT_TEMP',
@@ -36,22 +27,15 @@ commands = [
 ]
 
 conn = obd.OBD('socket://192.168.0.10:35000')
-# conn = obd.Async('/dev/ttyCAN0')
-# obd.logger.setLevel(obd.logging.DEBUG)
-# obd.logger.removeHandler(obd.console_handler)
 
 if conn.is_connected():
     file = open(f"logs/{car}_{datetime.now().strftime('%Y-%m-%d_%H-%M')}.log", 'w')
     print('\nCollecting...')
     try:
-        head = [c for c in commands]
-        file.write(','.join(head)+'\n')
-        # for c in commands:
-        #     conn.watch(obd.commands[c])
-        # conn.start()
+        file.write(','.join(commands)+'\n') # Head of csv
         time.sleep(1)
         while True:
-            print(datetime.now().time())
+            print(datetime.now().time()) # Just to see resulting interval
             log = []
             for c in commands:
                 try:
@@ -67,79 +51,21 @@ if conn.is_connected():
 else:
     print('Not connected')
 conn.close()
-# conn.stop()
 
 
 
+# def calculate_gear(self, rpm, speed):
+#     if speed == "" or speed == 0:
+#         return 0
+#     if rpm == "" or rpm == 0:
+#         return 0
+#     rps = rpm/60
+#     mps = (speed*0.44704) #meters per second
+#     final_drive  = 4.765
+#     tire_circumference = 1.964 #meters
+#     current_gear_ratio = (rps / (mps / tire_circumference)) / final_drive
+#     print(current_gear_ratio)
+#     gear = min((abs(current_gear_ratio - i), i) for i in self.gear_ratios)[1] 
+#     return gear
 
-# # Scheduler 
-# def repeat():
-#   threading.Timer(10.0, repeat).start()
-#   speedCmd = connection.query(obd.commands.SPEED)
-#   speedVal = str(speedCmd.value)
-#   fuelCmd = connection.query(obd.commands.FUEL_LEVEL)
-#   fuelVal = str(fuelCmd.value)
-#   print("Speed: " + speedVal + ", fuel: " + fuelVal)
-#   upload(speedVal, fuelVal)
-# repeat()
-
-
-# import obd_io
-# import serial
-# import platform
-# import obd_sensors
-# from datetime import datetime
-# import time
-
-# from obd_utils import scanSerial
-
-
-# class OBD_Recorder():
-#     def __init__(self, path, log_items):
-#         self.port = None
-#         self.sensorlist = []
-#         localtime = time.localtime(time.time())
-#         filename = path+"CivicSI-"+str(localtime[0])+"-"+str(localtime[1])+"-"+str(localtime[2])+"-"+str(localtime[3])+"-"+str(localtime[4])+"-"+str(localtime[5])+".log"
-# 	#filename = path+"1st-"+str(localtime[0])+"-"+str(localtime[1])+"-"+str(localtime[2])+"-"+str(localtime[3])+"-"+str(localtime[4])+"-"+str(localtime[5])+".log"
-# 	#filename = path+"2nd-"+str(localtime[0])+"-"+str(localtime[1])+"-"+str(localtime[2])+"-"+str(localtime[3])+"-"+str(localtime[4])+"-"+str(localtime[5])+".log"
-# 	#filename = path+"3rd-"+str(localtime[0])+"-"+str(localtime[1])+"-"+str(localtime[2])+"-"+str(localtime[3])+"-"+str(localtime[4])+"-"+str(localtime[5])+".log"
-# 	#filename = path+"4th-"+str(localtime[0])+"-"+str(localtime[1])+"-"+str(localtime[2])+"-"+str(localtime[3])+"-"+str(localtime[4])+"-"+str(localtime[5])+".log"
-
-#         self.log_file = open(filename, "w", 128)
-#         self.log_file.write("Time, RPM, MPH, short term fuel trim, long term fuel trim, Throttle, Gear\n");
-
-#         while 1:
-#             localtime = datetime.now()
-#             current_time = str(localtime.hour)+":"+str(localtime.minute)+":"+str(localtime.second)+"."+str(localtime.microsecond)
-#             log_string = current_time
-#             results = {}
-#             for index in self.sensorlist:
-#                 (name, value, unit) = self.port.sensor(index)
-#                 log_string = log_string + ","+str(value)
-#                 results[obd_sensors.SENSORS[index].shortname] = value;
-
-#             gear = self.calculate_gear(results["rpm"], results["speed"])
-#             log_string = log_string + "," + str(gear)
-#             self.log_file.write(log_string+"\n")
-            
-#     def calculate_gear(self, rpm, speed):
-#         if speed == "" or speed == 0:
-#             return 0
-#         if rpm == "" or rpm == 0:
-#             return 0
-
-#         rps = rpm/60
-#         mps = (speed*0.44704) #meters per second
-        
-#         final_drive  = 4.765
-        
-#         tire_circumference = 1.964 #meters
-
-#         current_gear_ratio = (rps / (mps / tire_circumference)) / final drive
-        
-#         print current_gear_ratio
-	
-# 	#gear = min((abs(current_gear_ratio - i), i) for i in self.gear_ratios)[1] 
-#         #return gear
-            
             
