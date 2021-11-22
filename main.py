3f1bb37a29b456c3274d89a463c0eaf998ba9990
Robson Caldas <p25@rcaldas.com>
2021-11-22 18:09:57 -0300

    Class OBD

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 from kivymd.app import MDApp
 from kivymd.tools.hotreload.app import MDApp
 from kivy.lang import Builder
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty
 from kivymd.uix.floatlayout import MDFloatLayout
+from obd2 import OBD
+import time
 
 class Connection(MDFloatLayout):
-    status = StringProperty('disconnected')
-
+    btn = StringProperty('Conectar')
+    status = StringProperty('desconectado')
+    connected = BooleanProperty(False)
+    obd = OBD()
+    def connect(self):
+        print('conectando')
+        self.btn = 'Conectando...'
+        self.status = 'conectando...'
+        time.sleep(2)
+        self.obd.connect()
+        self.show_conn()
+    def disconnect(self):
+        self.obd.disconnect()
+        self.show_conn()
+    def show_conn(self):
+        if self.obd.check_conn():
+            self.status = 'conectado'
+            self.btn = 'Desconectar'
+        else:
+            self.status = 'desconectado'
+            self.btn = 'Conectar'
 
 class OBD2(MDApp):
     KV_FILES = ['obd2.kv']
--- obd2.py
+++ obd2.py
@@ -3,75 +3,87 @@ import obd
 from datetime import datetime
 import time
 
-
-
-def loop_cmds(commands):
-    data = {}
-    for c in commands:
-        try:
-            data[c] = conn.query(obd.commands[c]).value.magnitude
-        except Exception as ex:
-            data[c] = False
-            print(f"Error in {c} command: {ex}")
-    return data
-
-def speed_view():
-    commands = ['RPM', 'SPEED', 'COOLANT_TEMP']
-    while True:
-        data = loop_cmds(commands)
-        if data['RPM'] == 0 or data['SPEED'] == 0:
-            data['RATIO'] = 0
+class OBD():
+    conn = False
+    def connect(self, type='wireless'):
+        if type == 'wireless':
+            self.conn = obd.OBD('socket://192.168.0.10:35000')
         else:
-            rps = data['RPM']/60
-            mps = data['SPEED']*0.277777
-            final_drive  = 2.87
-            tire_circumference = 2.085
-            data['RATIO'] = (rps / (mps / tire_circumference)) / final_drive
-            # gear = min((abs(current_gear_ratio - i), i) for i in gear_ratios)[1] 
-        for d in data.keys():
-            print(f'{d}: {data[d]}')
-
-def o2_view():
-    commands = [
-        'O2_S1_WR_CURRENT',
-        'O2_S5_WR_CURRENT',
-        'SHORT_FUEL_TRIM_1',
-        'SHORT_FUEL_TRIM_2',
-        'INTAKE_TEMP'
-    ]
-    while True:
-        data = loop_cmds(commands)
-        if data['O2_S1_WR_CURRENT'] < -0.01:
-            data['color'] = 'blue'
-        elif data['O2_S1_WR_CURRENT'] <= 0.01:
-            data['color'] = 'green'
+            print('not implemented')
+            self.conn = False
+    def check_conn(self):
+        if self.conn:
+            if self.conn.is_connected():
+                return True
+            else:
+                self.conn.close()
+                return False
         else:
-            data['color'] = 'red'
-        for d in data.keys():
-            print(f'{d}: {data[d]}')
+            print('false conn')
+            return False
+    def disconnect(self):
+        self.conn.close()
 
-car = 'Omega'
-def log_set(car, commands):
-    file = open(f"logs/{car}_{datetime.now().strftime('%Y-%m-%d_%H-%M')}.log", 'w')
-    file.write(','.join(commands)+'\n') # Head of csv
-    while True:
-        print(datetime.now().time())
-        data = loop_cmds(commands)
-        log = []
-        for d in data.values():
-            log.append(d)
-        file.write(','.join(log)+'\n')
+    def loop_cmds(self, commands):
+        data = {}
+        for c in commands:
+            try:
+                data[c] = self.conn.query(obd.commands[c]).value.magnitude
+            except Exception as ex:
+                data[c] = False
+                print(f"Error in {c} command: {ex}")
+        return data
 
-conn = obd.OBD('socket://192.168.0.10:35000')
-time.sleep(1)
-if conn.is_connected():
-    print('\nCollecting...')
-    try:
-        speed_view()
-        # log_set(o2Set)
-    except KeyboardInterrupt:
-        print("Stopped")
-    # file.close()
-else:
-    print('Not connected')
-conn.close()
+    def speed_view(self):
+        commands = ['RPM', 'SPEED', 'COOLANT_TEMP']
+        while True:
+            data = self.loop_cmds(commands)
+            if data['RPM'] == 0 or data['SPEED'] == 0:
+                data['RATIO'] = 0
+            else:
+                rps = data['RPM']/60
+                mps = data['SPEED']*0.277777
+                final_drive  = 2.87
+                tire_circumference = 2.085
+                data['RATIO'] = (rps / (mps / tire_circumference)) / final_drive
+                # gear = min((abs(current_gear_ratio - i), i) for i in gear_ratios)[1] 
+            for d in data.keys():
+                print(f'{d}: {data[d]}')
+
+    def o2_view(self):
+        commands = [
+            'O2_S1_WR_CURRENT',
+            'O2_S5_WR_CURRENT',
+            'SHORT_FUEL_TRIM_1',
+            'SHORT_FUEL_TRIM_2',
+            'INTAKE_TEMP'
+        ]
+        while True:
+            data = self.loop_cmds(commands)
+            if data['O2_S1_WR_CURRENT'] < -0.01:
+                data['color'] = 'blue'
+            elif data['O2_S1_WR_CURRENT'] <= 0.01:
+                data['color'] = 'green'
+            else:
+                data['color'] = 'red'
+            for d in data.keys():
+                print(f'{d}: {data[d]}')
+
+    def log_set(self, car, commands):
+        file = open(f"logs/{car}_{datetime.now().strftime('%Y-%m-%d_%H-%M')}.log", 'w')
+        file.write(','.join(commands)+'\n') # Head of csv
+        while True:
+            print(datetime.now().time())
+            data = self.loop_cmds(commands)
+            log = []
+            for d in data.values():
+                log.append(d)
+            file.write(','.join(log)+'\n')
+
+
+            # try:
+            #     speed_view()
+            #     # log_set(o2Set)
+            # except KeyboardInterrupt:
+            #     print("Stopped")
+            # file.close()
