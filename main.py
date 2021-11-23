3f02b9869a4691f56d12e246f677f94b44a646eb
Robson Caldas <bag@rcaldas.com>
2021-11-23 12:27:36 -0300

    One Class, Button change and obd2.py restore for manual use.

--- main.py
+++ main.py
@@ -1,38 +1,51 @@
 from kivymd.app import MDApp
 from kivymd.tools.hotreload.app import MDApp
 from kivy.lang import Builder
+from kivy.clock import Clock
+from functools import partial
 from kivy.properties import StringProperty, BooleanProperty
 from kivymd.uix.floatlayout import MDFloatLayout
-from obd2 import OBD
+import obd
 import time
+from datetime import datetime
 
 class Connection(MDFloatLayout):
     btn = StringProperty('Conectar')
     status = StringProperty('desconectado')
-    connected = BooleanProperty(False)
-    obd = OBD()
-    def connect(self):
-        print('conectando')
-        self.btn = 'Conectando...'
+    conn = False
+    def press(self, btn):
+        btn.disabled = True
+        btn.text = 'Conectando'
         self.status = 'conectando...'
-        time.sleep(2)
-        self.obd.connect()
-        self.show_conn()
-    def disconnect(self):
-        self.obd.disconnect()
-        self.show_conn()
-    def show_conn(self):
-        if self.obd.check_conn():
-            self.status = 'conectado'
-            self.btn = 'Desconectar'
-        else:
-            self.status = 'desconectado'
-            self.btn = 'Conectar'
+        Clock.schedule_once(partial(self.connect, 'wireless', btn), 0.359)
+    def connect(self, conntype, btn, *args):
+        if conntype == 'wireless':
+            self.conn = obd.OBD('socket://192.168.0.10:35000')
+        elif conntype == 'bluetooth':
+            print('not implemented')
+        if self.conn:
+            if self.conn.is_connected():
+                btn.txt = 'Desconectar'
+                btn.bind(on_release=self.disconnect)
+                btn.disabled = False
+                self.status = 'conectado'
+                return True
+            else:
+                self.conn.close()
+        btn.text = 'Conectar'
+        btn.disabled = False
+        self.status = 'desconectado'
+        return False
+    def disconnect(self, *args):
+        if self.conn and self.conn.is_connected():
+            self.conn.close()
+        self.status = 'desconectado'
+        self.btn = 'Conectar'
 
-class OBD2(MDApp):
-    KV_FILES = ['obd2.kv']
+class OBDII(MDApp):
+    # KV_FILES = ['obd2.kv']
     DEBUG = True
     def build_app(self):
         return Builder.load_file('obd2.kv')
 
-OBD2().run()
+OBDII().run()
